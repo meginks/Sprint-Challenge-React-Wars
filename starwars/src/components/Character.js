a80afa818f206c04f78864ea493e3c84c8e820a5
unknown <38929577+meginks@users.noreply.github.com>
2019-02-15 11:11:08 -0600

    all data is rendering to screen

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -3,7 +3,13 @@ import React from 'react';
 function Character(props) {
     return (
         <div className="character-box">
-        <h3>{props.starwarsChars}</h3>
+        <h1>{props.character.name}</h1>
+        <p>{props.character.height} centimeters tall</p>
+        <p>{props.character.mass} kg</p>
+        <p>Hair color: {props.character.hair_color}</p>
+        <p>Skin color: {props.character.skin_color}</p>
+        <p>Eye color: {props.character.eye_color}</p>
+        <p>Gender: {props.character.gender}</p>
         </div>
     )
 }
