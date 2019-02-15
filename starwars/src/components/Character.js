0c4161b9cd05c6c7f4fae921c2779e500c9f437b
unknown <38929577+meginks@users.noreply.github.com>
2019-02-15 10:41:24 -0600

    created character component

---
+++ starwars/src/components/Character.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Character(props) {
+    return (
+        <div className="character-box">
+        This is a character test. 
+        </div>
+    )
+}
+
+
+export default Character;
